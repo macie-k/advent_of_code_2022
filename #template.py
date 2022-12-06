a3a0bb1fd3c168a7c59e94382b76cb690029b0e7
macie.k <maciej.kazmierczykk@gmail.com>
2022-12-06 15:59:08 +0100

    :wrench: Update template

--- #template.py
+++ #template.py
@@ -1,16 +1,18 @@
-def pt1():
-    with open("./day_x_input.txt", "r") as f:
-        pass
+def pt1(lines):
+    pass
 
 
-def pt2():
-    with open("./day_x_input.txt", "r") as f:
-        pass
+def pt2(lines):
+    pass
 
 
 if __name__ == '__main__':
+    lines = []
+    with open("./day_x_input.txt", "r") as f:
+        lines = [line for line in f]
+
     print()
     print('--- DAY X ---')
-    print('1.', pt1())
-    print('2.', pt2())
+    print('1.', pt1(lines))
+    print('2.', pt2(lines))
     print()
